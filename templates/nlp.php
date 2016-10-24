a8362134ec1870b6625da2954c7f3ae3956ab0df
Manuel Enrique <menrique.gutierrez@gmail.com>
2016-10-24 16:57:01 -0500

    NLP: Bot Now Receive post data for processinge

--- templates/nlp.php
+++ templates/nlp.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../src/classes.php';
 
 $nlp = new NLP ; 
 
-$result=($nlp->NLPProcess("Cuando Llega el vuelo AV 12 de Miami?"));
+$result=($nlp->NLPProcess($message));
 
 header("Content-Type: application/json");
-print_r(json_encode($nlp->RedirectToFlow($result)));
+return(json_encode($nlp->RedirectToFlow($result)));
