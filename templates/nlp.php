c1a228f5361fd4455e2e7503b7736e72727c2933
Manuel Enrique <menrique.gutierrez@gmail.com>
2016-10-24 16:43:37 -0500

    add json response

--- templates/nlp.php
+++ templates/nlp.php
@@ -2,7 +2,8 @@
 require_once __DIR__ . '/../src/classes.php';
 
 $nlp = new NLP ; 
-echo "<pre>";
+
 $result=($nlp->NLPProcess("Cuando Llega el vuelo AV 12 de Miami?"));
-print_r($nlp->RedirectToFlow($result));
-echo "</pre>";
+
+header("Content-Type: application/json");
+print_r(json_encode($nlp->RedirectToFlow($result)));
